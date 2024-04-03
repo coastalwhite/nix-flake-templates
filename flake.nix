31d276bc5143d552daa072fda791330e27a06a51
coastalwhite <me@gburghoorn.com>
2024-04-03 10:56:02 +0200

    Add base template to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   outputs = { self, ... }: {
 	templates = {
+	  base = {
+		path = ./base;
+		description = "A small template with the system setup";
+	  };
 	  polars-py = {
 		path = ./polars-py;
 		description = "A simple polars python setup";
