aba229f7d24e319fb79841602868b7e2500e3ec6
coastalwhite <me@gburghoorn.com>
2024-04-05 11:00:40 +0200

    Remove follow for utils and properly import pkgs

--- base/flake.nix
+++ base/flake.nix
@@ -1,22 +1,18 @@
 {
-  description = "A very basic flake";
+  description = "A basic Nix Flake for eachDefaultSystem";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-
-    utils = {
-        url = "github:numtide/flake-utils";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; };
       in {
-          packages.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-          packages.default = self.packages.x86_64-linux.hello;
+          packages.hello = pkgs.hello;
+          packages.default = pkgs.hello;
       }
     );
 }
